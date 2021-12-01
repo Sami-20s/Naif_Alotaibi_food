3034a8ea5746348bda484518bfa9490f8d7b2595
Sami-20s <samikfouri22@gmail.com>
2021-12-01 22:25:44 +0200

    🧭 🔧 Making Link To Welcome Section

--- components/Hero/HeroButtons.js
+++ components/Hero/HeroButtons.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroButtons = () => {
     return (
-        <div className="hero__wrapper__buttons">
+        <div className="hero__wrapper__buttons " id="welcome">
             <button>OUR MENU</button>
         </div>
     );
