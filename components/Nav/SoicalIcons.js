1544559eacc61736952f48896218922f825cfe73
Sami-20s <samikfouri22@gmail.com>
2021-12-01 18:33:51 +0200

    🧭 🔧 Fixing Some Bugs in Nav

--- components/Nav/SoicalIcons.js
+++ components/Nav/SoicalIcons.js
@@ -5,13 +5,17 @@ import { FaTwitter, FaPinterest, FaInstagram } from "react-icons/fa";
 const SoicalIcons = () => {
     return (
         <div className="nav__wrapper__links__icons">
-            <a href="https://www.facebook.com" target="_blank">
+            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
                 <AiFillFacebook />
             </a>
-            <a href="https://twitter.com" target="_blank">
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
                 <FaTwitter />
             </a>
-            <a href="https://www.instagram.com" target="_blank">
+            <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noreferrer"
+            >
                 <FaInstagram />
             </a>
         </div>
