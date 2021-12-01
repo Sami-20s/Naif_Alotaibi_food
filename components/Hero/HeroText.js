4a7c67ba7ede9e56d7f4ec3e1b8bf56858b8180c
Sami-20s <samikfouri22@gmail.com>
2021-12-01 18:34:13 +0200

    🦸‍♀️ 🔧 Fixing Some Bugs in Hero

--- components/Hero/HeroText.js
+++ components/Hero/HeroText.js
@@ -3,9 +3,9 @@ import React from "react";
 const HeroText = () => {
     return (
         <div className="hero__wrapper__text">
-            <h1>It's a Food Adventure.</h1>
+            <h1>It&apos;s a Food Adventure.</h1>
             <p className="dots">- - - - - - -</p>
-            <p>You'll Never Put A Better Bit Of Food On Your Knife.</p>
+            <p>You&apos;ll Never Put A Better Bit Of Food On Your Knife.</p>
         </div>
     );
 };
